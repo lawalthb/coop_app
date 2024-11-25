d473ddc672c4ecf182b6252df6b60b22c0ff15a9
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-11-25 01:42:43 +0100

    total sum amount for dashbaord

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Loan;
+use App\Models\Saving;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class AdminDashboardController extends Controller
     public function index()
     {
         $totalMembers = User::where('is_admin', false)->count();
-        $totalSavings = Transaction::where('type', 'savings_deposit')->sum('credit_amount');
+        $totalSavings = Saving::sum('amount');
         $activeLoans = Loan::where('status', 'approved')->count();
         $pendingApprovals = User::where('admin_sign', 'No')->count();
 
